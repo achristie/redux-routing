c803babaec4151523dc200c64c02cb54ea2a236a
Andrew Christie <Andrew Christie>
2016-05-31 19:06:00 -0400

    finished

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
-
-const Greeting = () => {
-    return <div> Hey! </div>;
-};
-
+import PostsIndex from './components/posts-index';
+import PostsNew from './components/posts-new';
+import PostsShow from './components/posts-show';
 
 export default (
     <Route path='/' component={App}>
-        <Route path='greet' component={Greeting} />
-        <Route path='greet2' component={Greeting} />
-        <Route path='greet3' component={Greeting} />
+        <IndexRoute component={PostsIndex} />
+        <Route path='/posts/new' component={PostsNew} />
+        <Route path='/posts/:id' component={PostsShow} />
     </Route>
 );
